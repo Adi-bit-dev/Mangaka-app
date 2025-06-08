c353e1cfef1e3e702790bbf8a8cfb13ccbb55eee
Adi <azuredark647@gmail.com>
2025-06-08 17:54:06 +0530

    Added views and ratings to the trending styles

--- src/components/trending.jsx
+++ src/components/trending.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 import "./trendingStyles.css";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar as solidStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
+import { faStar as emptyStar } from '@fortawesome/free-regular-svg-icons';
+
 function Trending() {
+
+    function generateStarsJSX(rating) {
+        const fullStars = Math.floor(rating);
+        const hasHalfStar = rating % 1 >= 0.5;
+        const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
+
+        const stars = [];
+
+        for (let i = 0; i < fullStars; i++) {
+            stars.push(<FontAwesomeIcon icon={solidStar} key={`full-${i}`} />);
+        }
+
+        if (hasHalfStar) {
+            stars.push(<FontAwesomeIcon icon={faStarHalfAlt} key="half" />);
+        }
+
+        for (let i = 0; i < emptyStars; i++) {
+            stars.push(<FontAwesomeIcon icon={emptyStar} key={`empty-${i}`} />);
+        }
+
+        return stars;
+    }
+
     return (
         <div className="trending-container">
             <div className="header">New & Trending</div>
@@ -15,6 +42,16 @@ function Trending() {
                             <div className="trending-tag">Action</div>
                             <div className="trending-tag">Comedy</div>
                         </div>
+
+                        <div className="extra-info">
+                            <span className="stars spe-stars">{generateStarsJSX(2)}</span>
+                            <div className="view">
+                                <div className="view-icon">
+                                    <img src="../public/view.png" alt="V" />
+                                </div>
+                                <span>2.5k</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
@@ -25,6 +62,16 @@ function Trending() {
                         <div className="trending-card-tags">
                             <div className="trending-tag">Action</div>
                             <div className="trending-tag">Comedy</div>
+
+                        </div>
+                        <div className="extra-info">
+                            <span className="stars spe-stars">{generateStarsJSX(4.5)}</span>
+                            <div className="view">
+                                <div className="view-icon">
+                                    <img src="../public/view.png" alt="V" />
+                                </div>
+                                <span>3k</span>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -37,6 +84,15 @@ function Trending() {
                             <div className="trending-tag">Action</div>
                             <div className="trending-tag">Comedy</div>
                         </div>
+                        <div className="extra-info">
+                            <span className="stars spe-stars">{generateStarsJSX(3.5)}</span>
+                            <div className="view">
+                                <div className="view-icon">
+                                    <img src="../public/view.png" alt="V" />
+                                </div>
+                                <span>4.5k</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
@@ -47,6 +103,15 @@ function Trending() {
                         <div className="trending-card-tags">
                             <div className="trending-tag">Action</div>
                             <div className="trending-tag">Comedy</div>
+                        </div>
+                        <div className="extra-info">
+                            <span className="stars spe-stars">{generateStarsJSX(5)}</span>
+                            <div className="view">
+                                <div className="view-icon">
+                                    <img src="../public/view.png" alt="V" />
+                                </div>
+                                <span>18k</span>
+                            </div>
                         </div>
                     </div>
                 </div>
